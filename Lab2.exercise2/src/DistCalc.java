50165d68ecf14243f5bbe49f46dc3a35915e0306
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-06 02:05:44 -0500

    first commit

---
+++ Lab2.exercise2/src/DistCalc.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class DistCalc {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter x1");
+		double x1 = scan.nextDouble();
+		System.out.println("x1= "+ x1);
+		
+		System.out.println("Please enter y1");
+		double y1 = scan.nextDouble();
+		System.out.println("y1= "+ y1);
+		
+		System.out.println("Please enter x2");
+		double x2 = scan.nextDouble();
+		System.out.println("x2= "+ x2);
+		
+		System.out.println("Please enter y2");
+		double y2 = scan.nextDouble();
+		System.out.println("y2= "+ y2);
+		
+		double beforeSqrt = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
+		double sqrt = Math.sqrt(beforeSqrt);
+		
+		DecimalFormat fmt = new DecimalFormat("0.###");
+		
+		System.out.print("Distance between your coordinates is: ");
+		System.out.println(fmt.format(sqrt));
+	
+	}
+
+}
