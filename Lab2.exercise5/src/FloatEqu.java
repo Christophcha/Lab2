0b124784ee4aab2f049a37bc3d0f25d053364fe3
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-06 21:39:10 -0500

    added a tolerance

--- Lab2.exercise5/src/FloatEqu.java
+++ Lab2.exercise5/src/FloatEqu.java
@@ -2,8 +2,23 @@
 public class FloatEqu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		double x = (1.0/10) * (1.0/10);
+		double y = (1.0/100);
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		if (x == y) {
+			System.out.println("EQUAL"); 
+		}
+		if (Math.abs(x - y) < .00001) { 	//sets a tolerance between x and y
+			 System.out.println ("Essentially equal");
+		}
+		else {
+			System.out.println("NOT EQUAL");
+		}
 	}
-
 }
+
