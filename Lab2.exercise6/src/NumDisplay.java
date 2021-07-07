f2b5099e4e462f6c0377bc4e94b3dc4c7e332e4c
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-06 20:17:41 -0500

    first commit

---
+++ Lab2.exercise6/src/NumDisplay.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class NumDisplay {
+
+	public static void main(String[] args) {
+		
+	int number;
+	Scanner scan = new Scanner (System.in);
+	
+	System.out.print("Enter a number between 0-9: ");
+	number = scan.nextByte();
+	
+	switch(number) {
+		case 0:
+			System.out.print("Zero");	
+			break;
+		case 1:
+			System.out.print("One");
+			break;
+		case 2:
+			System.out.print("Two");
+			break;
+		case 3:
+			System.out.print("Three");
+			break;
+		case 4:
+			System.out.print("Four");
+			break;
+		case 5:
+			System.out.print("Five");
+			break;
+		case 6:
+			System.out.print("Six");
+			break;
+		case 7:
+			System.out.print("Seven");
+			break;
+		case 8:
+			System.out.print("Eight");
+			break;
+		case 9:
+			System.out.print("Nine");
+			break;
+		default:
+			System.out.print("You have entered a number that isnt between 0-9, try again.");
+			break;
+	}
+	}
+
+}
